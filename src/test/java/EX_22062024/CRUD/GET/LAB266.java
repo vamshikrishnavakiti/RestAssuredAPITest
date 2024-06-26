33aa4001e0d1edc7f15219b92df3a23a503606ff
Vamshi Krishna <vamshi.qaeng@gmail.com>
2024-06-26 22:35:26 +0530

    TestNG.Smoke file added

--- src/test/java/EX_22062024/CRUD/GET/LAB266.java
+++ src/test/java/EX_22062024/CRUD/GET/LAB266.java
@@ -1,5 +1,9 @@
 package EX_22062024.CRUD.GET;
 
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.Test;
+
 public class LAB266 {
     //Two ways to create a REST ASSURES TEst case
     // BDD Style- Given , When , Them
@@ -19,5 +23,21 @@ public class LAB266 {
     // Response Body
     // Time, Header, Cookies
 
-
+// NON BDD Style
+    @Test
+    public void getRequest(){
+        RequestSpecification rs = RestAssured.given();
+        rs.baseUri("http://restful-booker.herokuapp.com");
+        rs.basePath("/booker/2002").log().all();
+        rs.when().get();
+        rs.then().log().all().statusCode(200);
+    }
+    @Test
+    public void getNegetiveRequest(){
+        RequestSpecification rs = RestAssured.given();
+        rs.baseUri("http://restful-booker.herokuapp.com");
+        rs.basePath("/booker/-2002").log().all();
+        rs.when().get();
+        rs.then().log().all().statusCode(200);
+    }
 }
